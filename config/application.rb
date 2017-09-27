7b2fb89712f9dec8cdcf733b4f5f8bc8ac439663
Gustavo Rubio <gus@ahivamos.net>
2017-09-26 22:08:49 -0700

    - Enable UUID extensions for postgres  - Configure AR generators to use UUID for primary keys

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,9 @@ module Wallet
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |generator|
+      generator.orm :active_record, primary_key_type: :uuid
+    end
   end
 end
