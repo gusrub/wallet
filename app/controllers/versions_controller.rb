08a326e74b1ccb0a0143453f1abc1ee9bfe4e77e
Gustavo Rubio <gus@ahivamos.net>
2017-10-03 21:33:00 -0700

    - Disable authentication on root/versions endpoint

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -1,14 +1,16 @@
 class VersionsController < ApplicationController
 
+  skip_before_action :authenticate
+
   def index
     version = {
       level: "info",
       message: "This is an API application. Please check the documents to interact with it.",
       data: {
         version: {
-          major: 0,
-          minor: 1,
-          revision: 1
+          major: 1,
+          minor: 0,
+          revision: 0
         }
       }
     }
